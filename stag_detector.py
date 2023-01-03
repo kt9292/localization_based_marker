7e1ee4a58e4e0d301d0253c3918b13f9ebd7f671
kt9292 <kktae0034@gmail.com>
2023-01-03 09:56:49 +0900

    23.01.03

--- stag_detector.py
+++ stag_detector.py
@@ -28,10 +28,14 @@ def relativePosition(rvec1, tvec1, rvec2, tvec2):
 
 def find_stag(frame):
     grayimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # set Stag Detector parameter
     det = stag.Detector(11, 7 , False)
     det.detect(grayimg)
+    # get coners and ids from stag detected
     coners = det.getContours()
     ids = det.getIds()
+
     unique_ids = []
     unique_coners = []
 
@@ -50,6 +54,7 @@ def find_stag(frame):
     origin_center = []
     tvecs = []
     rvecs = []
+    
     for bbx, id in zip(unique_coners, unique_ids):
         top_left, top_right, bottom_right, bottom_left = bbx
         
@@ -59,11 +64,13 @@ def find_stag(frame):
         bottom_left = [int (x) for x in bottom_left]
         center = [int((top_left[0] + bottom_right[0]) / 2.0), int((top_left[1] + bottom_right[1]) / 2.0)]
 
-        bbx_for_axis = np.rint(np.array(bbx).reshape(1,4,2)) 
+        bbx_for_axis = np.rint(np.array(bbx).reshape(1,4,2))
+        # rvec : rotation vector , tvec : trasformation vector
+        # estimatePoseSingleMarkers(marker edge point, markerLength, intrinsic matrix, distortion matrix)
         rvec, tvec, marker_points = cv2.aruco.estimatePoseSingleMarkers(bbx_for_axis, 0.08, intrinsic_matrix, distortion_matrix)
-        # print(f'rvec:{rvec}\t rvec[0]:{rvec[0]}')
+
         tvec_added_id = np.append(tvec,id).reshape(1,1,4)
-        # print(f'{tvec_added_id.shape}\t {tvec_added_id}')
+
         tvecs.append(tvec_added_id)
         rvecs.append(rvec)
 
@@ -94,15 +101,14 @@ def cal_distance(frame, origin_center ,centers, tvecs, rvecs):
     
     for i in range(0, len(t)):
         if(int(t[i][0][0][3]) == 0):
+            # tvec0, rvec0 : transformation vectors of id 0
             tvec0 = t[i][:,:,:3]
             rvec0 = r[i]
 
-            for center, j in zip(centers, range(i+1, len(t))):    
+            for center, j in zip(centers, range(i+1, len(t))):
+                # tvec1, rvec1 : transformation vectors of id n    
                 tvec1 = t[j][:,:,:3]
                 rvec1 = r[j]
-
-                # calculate relative position of marker from origin marker
-                composedRvec, composedTvec = relativePosition(rvec0, tvec0, rvec1, tvec1)
 
                 tvec0_x = tvec0[0][0][0]
                 tvec0_y = tvec0[0][0][1]
@@ -110,15 +116,18 @@ def cal_distance(frame, origin_center ,centers, tvecs, rvecs):
                 tvec1_x = tvec1[0][0][0]
                 tvec1_y = tvec1[0][0][1]
                 tvec1_z = tvec1[0][0][2]
+
+                # calculate distacne between id:0 with id:n
                 dist1 = math.sqrt(pow((tvec0_x-tvec1_x),2)+pow((tvec0_y-tvec1_y),2)+pow((tvec0_z-tvec1_z),2))
                 distance= f'{dist1*100:.2f}cm'
-                # print(f'composedRvec\n{composedRvec}\ncomposedTvec\n{composedTvec[0][0]}\n')
-                # print(f'dist: {dist1}\n')
+                
+                # calculate relative position of marker from origin marker
+                composedRvec, composedTvec = relativePosition(rvec0, tvec0, rvec1, tvec1)
                 relative_position = []
                 for p in composedTvec:
                     relative_position.append(p[0])
                 relative_position_str = f'({relative_position[0]:.3f}, {relative_position[1]:.3f}, {relative_position[2]:.3f})'
-                # print(f'marker_id:{int(t[j][0][0][3])}\trelative_position:{relative_position_str}\tdistance:{distance}')
+        
                     
                 frame = cv2.putText(frame, distance,(int(abs(center[0]+origin_center[0])/2), int(abs(center[1] + origin_center[1])/2)),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2, cv2.LINE_4)
                 frame = cv2.putText(frame, relative_position_str, (int(center[0]), int(center[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 0, 0), 2, cv2.LINE_4)
@@ -129,11 +138,15 @@ def cal_distance(frame, origin_center ,centers, tvecs, rvecs):
     return frame
 
 
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+# camera config
 intrinsic_matrix = np.loadtxt("./config/intrinsic_matrix_logitech.txt", dtype=float)
 distortion_matrix = np.loadtxt("./config/distortion_matrix_logitech.txt", dtype=float)
+CAMERA_WIDTH = 1280
+CAMERA_HEIGHT = 720
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
 
 
 while(True):
